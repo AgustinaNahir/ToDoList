a95635a5eb9e4cbbb6f0c4e3065cf90d9239a819
agustinanahir <agustina.ferreira@live.com>
2022-05-29 16:36:24 -0300

    Creado el proyecto inical

---
+++ script.js
@@ -0,0 +1,9 @@
+const formulario = document.getElementById('formulario')
+const list = document.getElementById('list')
+const fragment = document.createDocumentFragment()
+let task = {}
+
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault()
+})
